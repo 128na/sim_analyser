44bbc3450915afa6a8f1ad3acc64e32a46004a6b
simutrans128na <simutrans.128na@gmail.com>
2016-07-08 21:25:04 +0900

    'fix_directory'

---
+++ src/lib/args.php
@@ -0,0 +1,41 @@
+<?php
+	/**
+	 * コマンドライン引数をいい感じに取得する
+	 * @since PHP5.4
+	 */
+class Args {
+
+	/**
+	 * オプションが指定されているかを返す
+	 * @param  string $option 探すオプション名
+	 * @return boolean オプションがあればtrue、なければfalse
+	 */
+	public static function has($option) {
+        global $argv;
+        foreach ($argv as $a) {
+            if ($a === $option) return true;
+        }
+        return false;
+	}
+
+	/**
+	 * オプションが指定されているかを返す
+	 * @param  string $option 探すオプション名
+	 * @return string|null オプションがあればその文字列、なければnullを返す
+	 */
+	public static function get($option) {
+        global $argv;
+        foreach ($argv as $i => $a) {
+            if ($a === $option) return self::exists($argv[$i+1]);
+        }
+        return null;
+	}
+
+	/**
+	 * PHP7から実装された??演算子の代わり
+	 */
+	private static function exists(&$item, $default = null) {
+		return isset($item) ? $item : $default;
+	}
+
+}
